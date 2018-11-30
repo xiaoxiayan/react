8594d57ed59a1b8b2b055d2ef2abb6b03e0e873d
LLLQW <a690150618@qq.com>
2018-11-30 14:41:13 +0800

    创建

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+         <h1>开始</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
